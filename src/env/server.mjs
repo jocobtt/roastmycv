aac3f67b3e98803283734943da8b653ba8340b08
Isaac <asterixandisaac@gmail.com>
2023-03-04 09:07:17 -0700

    remove strict env check

--- src/env/server.mjs
+++ src/env/server.mjs
@@ -8,13 +8,13 @@ import { env as clientEnv, formatErrors } from "./client.mjs";
 
 const _serverEnv = serverSchema.safeParse(process.env);
 
-if (!_serverEnv.success) {
-  console.error(
-    "❌ Invalid environment variables:\n",
-    ...formatErrors(_serverEnv.error.format()),
-  );
-  throw new Error("Invalid environment variables");
-}
+// if (!_serverEnv.success) {
+//   console.error(
+//     "❌ Invalid environment variables:\n",
+//     ...formatErrors(_serverEnv.error.format()),
+//   );
+//   throw new Error("Invalid environment variables");
+// }
 
 for (let key of Object.keys(_serverEnv.data)) {
   if (key.startsWith("NEXT_PUBLIC_")) {
