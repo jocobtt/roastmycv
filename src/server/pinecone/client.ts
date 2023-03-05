a008769564b17e8a499c857fd5bca54e6901312d
Isaac <asterixandisaac@gmail.com>
2023-03-05 12:39:33 -0700

    quite down

--- src/server/pinecone/client.ts
+++
@@ -1,9 +0,0 @@
-import { PineconeClient } from "pinecone-client";
-
-import { env } from "../../env/server.mjs";
-
-export const pinecone = new PineconeClient({
-  apiKey: env.PINECONE_API_KEY,
-  baseUrl: env.PINECONE_BASE_URL,
-  namespace: "",
-});
