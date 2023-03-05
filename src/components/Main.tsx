31823303b2090896a6c0dfebb9911b7699c3b0a6
Isaac <asterixandisaac@gmail.com>
2023-03-05 10:26:01 -0700

    change the main component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,19 +4,19 @@ const Main = ({ children }: { children: React.ReactNode }) => {
   const { data: authSessionData } = useSession();
 
   return (
-    <div className="h-screen bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+    <div className="h-screen bg-gradient-to-tl from-rose-100 to-teal-100">
       <nav className="flex">
-        <p className="px-10 py-3 text-xl text-white">
-          Chat<span className="text-search">BOM</span>
+        <p className="px-10 py-3 text-xl text-black">
+          Chat<span className="text-grey">BOM</span>
         </p>
-        <div className="ml-auto">
+        {/* <div className="ml-auto">
           <button
             className="bg-gray/10 hover:bg-gray/20 rounded-full px-10 py-3 font-semibold text-white no-underline transition"
             onClick={authSessionData ? () => signOut() : () => signIn("google")}
           >
             {authSessionData ? "Sign out" : "Sign in"}
           </button>
-        </div>
+        </div> */}
       </nav>
       <div className="mx-4 px-4 py-16 text-center">{children}</div>
     </div>
