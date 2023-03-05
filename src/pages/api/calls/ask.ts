2bbf4ef316aa9f74733c01731e9f8b059bc0dcc9
Isaac <asterixandisaac@gmail.com>
2023-03-05 14:10:55 -0700

    add error stuff

--- src/pages/api/calls/ask.ts
+++ src/pages/api/calls/ask.ts
@@ -24,49 +24,58 @@ function isJsonString(str: string) {
 
 const ask = async (req: NextApiRequest, res: NextApiResponse) => {
   // Process question
+  let stage = 0
   try {
     let body = req.body;
+    stage = 1
     if (isJsonString(body)) {
       body = JSON.parse(req.body);
     }
 
+    stage = 2
     const parsed = questionSchema.parse(body);
+    stage = 3
     const { text } = parsed;
-
-    console.log("hi 1");
+    
     // Embed question text
     // const response = await openai.createEmbedding({
-    //     input: text,
-    //     model: "text-embedding-ada-002",
-    // });
+      //     input: text,
+      //     model: "text-embedding-ada-002",
+      // });
+    stage = 4
     const response = await createEmbedding(text);
-
+    stage = 5
+    
+    stage = 6
     const { embedding, index } = response.data[0] as {
       embedding: number[];
       index: number;
     };
 
     // Get top 5 most relevant verses
+    stage = 7
     const similarEmbeddings = await query({
       vector: embedding,
       topK: 5,
       includeMetadata: true,
     });
-
+    
+    stage = 8
     const answers = (
       similarEmbeddings as {
         matches: {
           metadata: Record<string, unknown>;
         }[];
       }
-    ).matches.map((match) => {
-      return match.metadata;
-    });
-
+      ).matches.map((match) => {
+        return match.metadata;
+      });
+      
+    stage = 9
     res.status(200).json(answers);
   } catch (e) {
     console.log(e);
-    res.status(400).json({ error: (e as Error).message, body: req.body });
+    res.status(400).json({ error: (e as Error).message, body: req.body, stage: stage });
   }
 };
 
