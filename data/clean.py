e323a381c7d49adeee8d381246dce774b7924a71
Jacob Braswell <jacob.braswell@collibra.com>
2023-03-06 20:05:56 -0500

    fixes to huggingface data
    Now pulling the data from huggingface. Fixed empty index being added to the data and am now pulling data from huggingface for creation of the embeddings.

--- data/clean.py
+++ data/clean.py
@@ -1,26 +1,38 @@
 import pandas as pd 
 import os 
 from datasets import load_dataset 
+import numpy as np 
 
 def clean_data(data):
     df = pd.read_csv(data)
     # Drop all data that where book isn't equal to Book of Mormon
     df = df[df['volume_title'] == "Book of Mormon"]
-    # split into test and train sets 
+    # make a test set 
+    df["train_test"] = np.random.choice(["train", "test"], size = len(df), p = [0.8, 0.2])    
+    # split into train and test
+    train = df[df["train_test"] == "train"]
+    test = df[df["train_test"] == "test"]
+    # drop train_test column
+    train = train.drop(columns=['train_test'])
+    test = test.drop(columns=['train_test'])
+    df = df.drop(columns = ['train_test'])
 
     # output df to csv 
-    df.to_csv('lds-scriptures-cleaned.csv', index=True)
+    df.to_csv('lds-scriptures-cleaned.csv', index=False)
+    train.to_csv('bom_train.csv', index=False)
+    test.to_csv('bom_test.csv', index=False)
     return df.head()
 
-def upload_to_huggingface(data):
+def upload_to_huggingface():
     # log into huggingface 
     #os.system('huggingface-cli login')
     # Upload to huggingface
-    dataset = load_dataset('csv', data_files=data)
+    data_files = {"full_data": "lds-scriptures-cleaned.csv", "train": "bom_train.csv", "test": "bom_test.csv"}
+    dataset = load_dataset('csv', data_files=data_files)
     dataset.push_to_hub('jbrazzy/book_of_mormon_corpus')
 
 if __name__ == '__main__':
     # Clean data
     clean_data('lds-scriptures.csv')
     # push to huggingface
-    upload_to_huggingface('lds-scriptures-cleaned.csv')
+    upload_to_huggingface()
