5a3986debc867b69f6fb93021146d39fd9d72059
Isaac Z Tai <39180701+powerpufffs@users.noreply.github.com>
2023-03-09 17:21:27 -0700

    finish roast my resume

---
+++ src/pages/api/calls/roast.ts
@@ -0,0 +1,112 @@
+import { type NextApiRequest, type NextApiResponse } from "next";
+import * as z from "zod";
+import { pinecone, query, get } from "../../../utils/pinecone";
+import { createEmbedding, openai } from "../../../utils/openai";
+import { ChatCompletionRequestMessage } from "openai";
+import axios from "axios";
+
+const questionSchema = z.object({
+  text: z.string().min(2, "Please enter more than 1 character"),
+  prevRoasts: z.optional(z.array(z.string())),
+});
+
+function isJsonString(str: string) {
+  try {
+    JSON.parse(str);
+  } catch (e) {
+    return false;
+  }
+  return true;
+}
+
+const ask = async (req: NextApiRequest, res: NextApiResponse) => {
+  // Process question
+  let stage = 0;
+  try {
+    let body = req.body;
+    stage = 1;
+    if (isJsonString(body)) {
+      body = JSON.parse(req.body);
+    }
+
+    stage = 2;
+    // Construct the prompt
+    const prompt = `
+      You are the world's foremost expert on script writing and copywriting. A few years ago a project called pudding.cool went viral online for roasting people's music listening habits.
+
+      Your job is to create a version of this that is suitable for roasting career resumes and CVs.
+
+      Tone should be extremely passive aggressive, monotone. Act bored, as if you do this all day and can’t wait to go home. 
+      
+      ONE SENTENCE MAXIMUM. Shorter sentences are better. Do NOT mention any human names in your ouptut. DO mention company names where appropriate.
+
+      Make every roast unique. Be careful not to repeat formats or jokes.
+    
+      ${body.text}
+
+      Here are some examples roasts based on the text above:
+      roast: worked at google? classic...
+      roast: you switch jobs more than my mum switch clothes. I guess that's okay nowadays...
+      roast: is "Founder" codename for "unemployed"? I wish you the best I guess...
+      roast: it's almost like you enjoy using the words "delivered", "authored" and "built". Want some resume with your buzzwords?
+      roast: brigham young university? I bet their parties are fun...
+      ${body.prevRoasts?.map((roast) => `roast: ${roast}`).join("\n")}
+    `;
+
+    console.log(prompt);
+
+    // const questions: ChatCompletionRequestMessage[] = [
+    //   {
+    //     role: "user",
+    //     content: "Give me a 10 word summary of the Book of Mormon?",
+    //   },
+    //   {
+    //     role: "assistant",
+    //     content:
+    //       "Sacred text of Latter-day Saints about ancient Americas and Jesus Christ.",
+    //   },
+    // ];
+
+    stage = 3;
+    console.log("trying");
+
+    const openAIRes = await axios.post(
+      "https://api.openai.com/v1/chat/completions",
+      {
+        model: "gpt-3.5-turbo",
+        messages: [
+          { role: "system", content: prompt },
+          { role: "user", content: "roast: " },
+        ],
+        temperature: 0.9,
+        max_tokens: 150,
+        top_p: 1,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.6,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+          Accept: "application/json",
+        },
+      }
+    );
+
+    stage = 11;
+    const answer = openAIRes.data.choices[0].message.content;
+    stage = 12;
+
+    stage = 13;
+    res.status(200).json({ answer: answer });
+  } catch (e) {
+    console.log(e.message);
+    res.status(400).json({
+      error: e,
+      message: (e as Error).message,
+      body: req.body,
+      stage: stage,
+    });
+  }
+};
+
+export default ask;
---
+++ src/pages/roast.tsx
@@ -0,0 +1,226 @@
+import React from "react";
+import axios from "axios";
+
+import Main from "../components/Main";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { RingLoader } from "react-spinners";
+import { AnimatePresence, motion } from "framer-motion";
+import Head from "next/head";
+import { NextPage } from "next";
+import { Delay } from "../components/animation/Delay";
+
+const ScriptLine = ({ text }) => {
+  return (
+    <section className="flex flex-col rounded-md bg-white/50 p-2 text-left">
+      <p className="mt-2 whitespace-pre-line">{text}</p>
+    </section>
+  );
+};
+
+const Roast = ({ id, resume, prevRoasts, addRoast }) => {
+  const ref = React.useRef<HTMLParagraphElement>();
+  const { status, data } = useQuery(
+    ["question", id],
+    async () => {
+      const res = await axios.post("/api/calls/roast", {
+        text: resume,
+        prevRoasts: prevRoasts,
+      });
+      return res.data;
+    },
+    {
+      refetchOnWindowFocus: false,
+    }
+  );
+
+  React.useEffect(() => {
+    const scrollingElement = document.scrollingElement || document.body;
+    scrollingElement.scrollTop = scrollingElement.scrollHeight;
+  }, []);
+
+  const text = React.useMemo(() => {
+    if (status === "loading") {
+      return "...";
+    }
+    if (status === "success") {
+      ref.current.scrollIntoView();
+    }
+    const lower = data.answer.charAt(0).toLowerCase() + data.answer.slice(1);
+    return lower;
+  }, [status, data]);
+
+  React.useEffect(() => {
+    if (text !== "..." && prevRoasts.includes(text) === false) {
+      addRoast(text);
+    }
+  }, [addRoast, prevRoasts, text]);
+  return (
+    <section className="flex flex-col rounded-md bg-white/50 p-2 text-left lowercase">
+      <p ref={ref}>{text}</p>
+    </section>
+  );
+};
+
+const script = [
+  "loading your career history...",
+  "analyzing your work experience...",
+  "...this is a lot.",
+  "[[ROAST1]]",
+  "we're off to a great start...",
+  "[[ROAST2]]",
+  `this is my favorite thing to read on a ${new Date().toLocaleString("en-us", {
+    weekday: "long",
+  })}...`,
+  "[[ROAST3]]",
+  "are you sure you didn't just copy-paste this resume from a LinkedIn template?",
+  "[[ROAST4]]",
+  "I'm not saying I'm bored but....",
+  "[[ROAST5]]",
+  "that's it. i'm done.",
+  "i guess the important thing is that you believe in yourself...",
+  "bye. i can't wait to do this again",
+];
+
+const Handbook: NextPage = () => {
+  const [question, setQuestion] = React.useState("");
+  const [status, setStatus] = React.useState("idle");
+  const [show, setShow] = React.useState(false);
+  const [prevRoasts, setPrevRoasts] = React.useState([]);
+  const searchRef = React.useRef<HTMLTextAreaElement>();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setStatus("loading");
+  };
+
+  React.useEffect(() => {
+    if (status === "loading") {
+      setTimeout(() => {
+        setStatus("success");
+      }, 2000);
+    }
+  }, [status]);
+
+  const handleKeyDown = (e) => {
+    if (e.which === 13 && !e.shiftKey) {
+      handleSubmit(e);
+    }
+  };
+
+  React.useEffect(() => {
+    searchRef.current.focus();
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Roast My Resume</title>
+        <meta name="description" content="Talk to the scriptures." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="min-h-screen bg-gradient-to-tl from-rose-100 to-teal-100 pt-8 pb-4">
+        <nav className="flex justify-center">
+          <p className="5x-10 max-w-sm py-3 text-3xl text-black">
+            Roast my resume. Please.
+          </p>
+          {/* <div className="ml-auto">
+          <button
+            className="bg-gray/10 hover:bg-gray/20 rounded-full px-10 py-3 font-semibold text-white no-underline transition"
+            onClick={authSessionData ? () => signOut() : () => signIn("google")}
+          >
+            {authSessionData ? "Sign out" : "Sign in"}
+          </button>
+        </div> */}
+        </nav>
+        <section className="mt-1 flex w-screen flex-col items-center">
+          <section className="flex w-96 flex-col px-3">
+            <form onSubmit={handleSubmit} className="mb-4">
+              <textarea
+                ref={searchRef}
+                onKeyDown={handleKeyDown}
+                onChange={(e) => setQuestion(e.target.value)}
+                placeholder="Input your resume here."
+                className="flex h-24 w-full resize-none flex-col rounded-md border-2 border-black p-2 text-xl"
+              />
+              {status === "idle" ? (
+                <button
+                  className="mt-2 h-12 w-full rounded-md border-2 border-black text-xl hover:bg-black hover:text-white"
+                  disabled={status === "loading"}
+                  type="submit"
+                >
+                  roast me, I guess.
+                </button>
+              ) : (
+                <>{null}</>
+              )}
+            </form>
+            {/* <button onClick={() => setShow(true)}>Start</button> */}
+            <AnimatePresence>
+              {status === "loading" ? (
+                <motion.div
+                  key="loading"
+                  className="flex justify-center"
+                  initial={{ y: 50, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  exit={{ y: 50, opacity: 0 }}
+                >
+                  <RingLoader size={80} className="" />
+                </motion.div>
+              ) : status === "success" ? (
+                <motion.div key="joy">
+                  {/* <section className="flex flex-col rounded-md bg-white/50 p-2 text-left">
+                    <p>🤖 ChatBOM:</p>
+                    <i className="mt-4">{answer}</i>
+                  </section>
+                  <p className="mt-4">Sections that may be relevant: </p> */}
+                  <AnimatePresence>
+                    {/* <Delay key="relevant-sections" delay={1000}> */}
+                    <motion.div
+                      initial={{ y: 100, opacity: 0 }}
+                      animate={{ y: 0, opacity: 1 }}
+                    >
+                      {script.map((answer, index) => {
+                        return (
+                          <motion.div
+                            key={answer}
+                            initial={{ y: 100, opacity: 0 }}
+                            animate={{ y: 0, opacity: 1 }}
+                            transition={{
+                              y: 100,
+                              duration: 0.5,
+                              delay: 7 * index,
+                            }}
+                            className="mt-2"
+                          >
+                            {answer.includes("ROAST") ? (
+                              <Roast
+                                id={index}
+                                resume={question}
+                                prevRoasts={prevRoasts}
+                                addRoast={(roast) =>
+                                  setPrevRoasts((prev) => [...prev, roast])
+                                }
+                              />
+                            ) : (
+                              <ScriptLine key={index} text={answer} />
+                            )}
+                          </motion.div>
+                        );
+                      })}
+                    </motion.div>
+                    {/* </Delay> */}
+                  </AnimatePresence>
+                </motion.div>
+              ) : (
+                <></>
+                // <pre className="mt-4">{data}</pre>
+              )}
+            </AnimatePresence>
+          </section>
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default Handbook;
