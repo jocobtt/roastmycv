52f79655f0a7b7ad2858eeebc3ab5e3143883e0b
Isaac Z Tai <39180701+powerpufffs@users.noreply.github.com>
2023-03-11 12:13:50 -0700

    fix typo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -251,7 +251,7 @@ const Handbook: NextPage = () => {
                     {!file && <p className="pl-1">or drag and drop</p>}
                   </div>
                   <p className="text-xs text-gray-500">
-                    PNG, JPG, GIF up to 10MB
+                    PDF up to 10MB
                   </p>
                 </div>
               </div>
